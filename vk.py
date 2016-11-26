5dc9480255f5d187d223841d21117f6cfc75fca8
ewigkeit <ewigkeit13.13@gmail.com>
2016-11-26 20:39:45 +0800

    2016-11-26 def auth

--- vk.py
+++ vk.py
@@ -12,22 +12,11 @@ def start():
            f = open('/usr/local/my_vk/vk.conf','w')
            print('Готово, чувак!')
 
-
-
-
-
-'''
-    def get_key(self):
         key = input("Введите ваш ключ \n"
                     "Очень просто получить ключ, для этого нужно пройти по ссылке: \n"
                     "https://oauth.vk.com/authorize?client_id=5725898&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,photos,audio,video,docs,notes,pages,status,wall,groups,messages,notifications,offline&response_type=token")
 
 
 
-
-    def incode(self, ) :
-    #    print("var--->", va)
-
-'''
 start()
 
